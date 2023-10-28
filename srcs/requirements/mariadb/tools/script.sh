af9670f05a4cd5af55372e855c588f0c37df934b
vgejno <vgejno@student.42lausanne.ch>
2023-10-29 01:30:17 +0200

    mariadb working

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,18 +1,38 @@
 #!/bin/sh
 
-mysql_install_db
+# Check if the data directory exists
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+fi
 
-mysqld --user=mysql &
+# Start MariaDB in the background
+mysqld --user=mysql --datadir=/var/lib/mysql &
 
-# Wait for MariaDB to start
-while ! mysqladmin ping -h127.0.0.1 --silent; do
+# Store the PID of the mysqld process
+mysql_pid=$!
+
+# Wait for MariaDB to become available
+until mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} ping >/dev/null 2>&1; do
     sleep 1
 done
 
-echo "GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot -p
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root -p
+# "ALTER USER 'root'@'localhost' IDENTIFIED BY 'your_new_password';"
 
-# Do not stop the MariaDB service here, let Docker manage it
-# /etc/init.d/mysql stop
+# ADMIN_PASSWORD=$(openssl passwd -1 "$WP_ADMIN_PASSWORD" | sed 's/\//\\\//g')
+# PASSWORD=$(openssl passwd -1 "$WP_PASSWORD" | sed 's/\//\\\//g')
 
-exec "$@"
+# # Initialize the database and permissions
+# sudo mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot -p${MYSQL_ROOT_PASSWORD}
+# sudo mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot -p${MYSQL_ROOT_PASSWORD}
+
+if [ $? -ne 0 ]; then
+         sudo mysql -u root -p${MYSQL_ROOT_PASSWORD} $MYSQL_DATABASE
+fi
+
+# Wait for the MariaDB process to shut down gracefully
+wait "$mysql_pid"
+
+# Start MariaDB in the foreground
+exec mysqld --user=mysql
--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -2,20 +2,17 @@
 # set -x
 # sleep 10
 
-
 if [ ! -f /var/www/html/wp-config.php ]; then
     wget https://wordpress.org/latest.tar.gz -P /var/www/html \
     && cd /var/www/html && tar -xzf latest.tar.gz && rm latest.tar.gz
     wp core download --allow-root
     wp config create --allow-root \
-     --dbname=$MYDB_NAME \
-     --dbuser=$MYDB_USER \
-     --dbpass=$MYDB_PWD \
-     --dbhost=$MYDB_HOST:3306 --path='/var/www/html'
-     #--dbhost="$MYSQL_HOSTNAME" \
-
+     --dbname=$WORDPRESS_DB_NAME \
+     --dbuser=$WORDPRESS_DB_USER \
+     --dbpass=$WORDPRESS_DB_PASSWORD \
+     --dbhost=$WORDPRESS_DB_HOST:3306 --path='/var/www/html'
     wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --allow-root --path='/var/www/html'
-    wp user create $DB_USER $DB_EMAIL --user_pass=$WP_PWD --role=author --allow-root --path='/var/www/html'
+    wp user create $WORDPRESS_DB_USER $DB_EMAIL --user_pass=$WP_PWD --role=author --allow-root --path='/var/www/html'
     wp theme install astra --activate --allow-root --path='/var/www/html'
     # Other installation steps
 else
@@ -23,12 +20,12 @@ else
 fi
 
 # Check if WP-CLI configuration was successful
- if [ $? -eq 0 ]; then
+if [ $? -eq 0 ]; then
      echo "WordPress configuration was successful."
- else
+else
      echo "WordPress configuration failed."
      exit 1  # You can choose to exit the script or handle errors differently
- fi
+fi
 
 # Start PHP-FPM
 if [ ! -d /run/php ]; then
