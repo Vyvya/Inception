f26fb79fe0b8ddd7f8989f9a8e808040cd79053f
vgejno <vgejno@student.42lausanne.ch>
2023-10-31 16:06:32 +0100

    m

--- Inception_new/srcs/requirements/mariadb/tools/script.sh
+++ Inception_new/srcs/requirements/mariadb/tools/script.sh
@@ -1,18 +1,58 @@
 #!/bin/sh
 
-chown -R mysql:mysql /var/lib/mysql
-
 # Check if the data directory exists
-if [ ! -d "/var/lib/mysql/wordpress" ]; then
+if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
-    service mysql start
     service mysql start
     mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME DEFAULT CHARACTER SET utf8;"
     mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
     mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
+    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
     mysql -u root -e "FLUSH PRIVILEGES;"
     mysqladmin -u root password $DB_ROOT_PASSWORD
     service mysql stop
 fi
 
+# Start MariaDB with TCP protocol
 /usr/bin/mysqld_safe --bind-address=0.0.0.0
+
+#--protocol=TCP
+
+
+
+
+
+
+
+
+
+#!/bin/sh
+
+# mkdir -p /var/lib/mysql /var/run/mysqld
+
+# chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+# chmod -R 755 /var/lib/mysql /var/run/mysqld
+
+# touch /var/run/mysqld/mysqld.pid
+# touch /var/run/mysqld/mysqld.sock
+
+# chown -R mysql:mysql /var/run/mysqld/mysqld.sock
+# chown -R mysql:mysql /var/run/mysqld/mysqld.pid
+# chmod -R 644 /var/run/mysqld/mysqld.sock
+
+# Check if the data directory exists
+# if [ ! -d "/var/lib/mysql/mysql" ]; then
+#     mysql_install_db --user=mysql --datadir=/var/lib/mysql
+#     service mysql start
+#     mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME DEFAULT CHARACTER SET utf8;"
+#     mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+#     mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
+#     mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+#     mysql -u root -e "FLUSH PRIVILEGES;"
+#     mysqladmin -u root password $DB_ROOT_PASSWORD
+#     service mysql stop
+# fi
+
+# # systemctl enable mariadb
+# # /usr/bin/mysqld_safe --bind-address=0.0.0.0
+# /etc/init.d/mysql start --protocol=TCP
--- Inception_new/srcs/requirements/wordpress/conf/auto_config.sh
+++ Inception_new/srcs/requirements/wordpress/conf/auto_config.sh
@@ -2,18 +2,11 @@
 
 sleep 5
 
-if [ ! -f /var/www/html/wp-config.php ]; then
-    wget https://wordpress.org/latest.tar.gz -P /var/www/ \
-    && cd /var/www/ && tar -xzf latest.tar.gz && rm latest.tar.gz
-    wp core download --allow-root
-    wp config create --allow-root \
-     --dbname=$DB_NAME \
-     --dbuser=$DB_USER \
-     --dbpass=$DB_PASSWORD \
-     --dbhost=$DB_HOST --path='/var/www/html'
-    wp core install --url=$DOMAIN_NAME --title=$TITLE --admin_user=$USER_NAME --admin_password=$DB_ROOT_PASSWORD --admin_email=$USER_ADMIN_EMAIL --skip-email --allow-root --path='/var/www/html'
-    wp user create $DB_USER $USER_EMAIL --user_pass=$DB_PASSWORD --role=suscriber --allow-root --path='/var/www/html'
-    wp theme install astra --activate --allow-root --path='/var/www/html'
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
+    wp core config --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --alow-root
+    wp core install --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${USER_NAME} --admin_password=${DB_ROOT_PASSWORD} --admin_email=${USER_ADMIN_EMAIL} --skip-email --allow-root
+    wp user create ${DB_USER} ${USER_EMAIL} --user_pass=${DB_PASSWORD} --role=suscriber --allow-root
+    # wp theme install astra --activate --allow-root
     # Other installation steps
 else
     echo "WordPress is already installed."
@@ -32,6 +25,6 @@ fi
 #     mkdir -p /run/php
 # fi
 
-/usr/sbin/php-fpm7.3 -F
+/usr/sbin/php-fpm7.3 -F  --nodaemonize
 
 # exec "$@"
