5398e6f6c21a70fc66f1c238c2478b0dc3fe3507
vgejno <vgejno@student.42lausanne.ch>
2023-11-05 23:56:05 +0100

    final

--- Inception_new/srcs/requirements/nginx/tools/nginx_script.sh
+++ Inception_new/srcs/requirements/nginx/tools/nginx_script.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# sed -i "s/{DOMAIN_NAME}/${DOMAIN_NAME}/g" /etc/nginx/nginx.conf
-
 if [ -z "$(ls -A "/etc/ssl/certs")" ]; then
 	openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 		-keyout /etc/ssl/private/nginx-selfsigned.key \
@@ -10,5 +8,4 @@ if [ -z "$(ls -A "/etc/ssl/certs")" ]; then
 
 fi
 
-# mv /conf/nginx.conf /etc/nginx/sites-available/default
 exec "$@"
--- Inception_new/srcs/requirements/wordpress/tools/auto_config.sh
+++ Inception_new/srcs/requirements/wordpress/tools/auto_config.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-rm -f /var/www/wordpress/wp-config.php
+sleep 5
+# rm -f /var/www/wordpress/wp-config.php
         
-wp core download --allow-root
-wp config create --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --skip-check --allow-root
-wp core install --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${USER_ADMIN_NAME} --admin_password=${USER_ADMIN_PASSWORD} --admin_email=${USER_ADMIN_EMAIL} --allow-root
+# wp core download --allow-root
+# wp config create --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --skip-check --allow-root
+wp core install --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${USER_ADMIN_NAME} --admin_password=${USER_ADMIN_PASSWORD} --admin_email=${USER_ADMIN_EMAIL} --skip-email --allow-root
 wp user create ${USER_NAME} ${USER_EMAIL} --user_pass=${USER_PASSWORD} --role=subscriber --allow-root
 wp theme install divi --activate --allow-root
 
