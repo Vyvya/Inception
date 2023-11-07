992ae5937a78a2e7409ed02c1f3cb186c06acd16
vgejno <vgejno@student.42lausanne.ch>
2023-11-07 12:09:58 +0100

    single inception folder

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
 all:
 	docker compose -f ./srcs/docker-compose.yml up -d --build
 
+nc: nocache up
+
 up:
 	docker compose -f ./srcs/docker-compose.yml up -d
 
-build:
-	docker compose ./srcs/docker-compose.yml up -d --build
+nocache:
+	docker compose -f ./srcs/docker-compose.yml build --no-cache
 
 down:
 	docker compose -f ./srcs/docker-compose.yml down
@@ -20,83 +22,4 @@ clean:
 
 re: down clean all
 
-.PHONY: up down build
-
-# include srcs/.env
-# export
-
-# COMPOSE_DIR = srcs
-# COMPOSE_FILE = $(COMPOSE_DIR)/docker-compose.yml
-
-# BIN = docker compose
-# ARGS = -f $(COMPOSE_FILE)
-# DKC = $(BIN) $(ARGS)
-# DKC_CONFIG = $(DKC) config
-
-# COMPOSE_PROJECT_NAME ?= $(COMPOSE_DIR)
-# HOST_VOLUME_ROOT ?= /home/$(USER)/data
-
-# VOLUME_MOUNTS = wordpress mariadb
-# VOLUME_PATHS = $(addprefix $(HOST_VOLUME_ROOT)/, $(VOLUME_MOUNTS))
-# VOLUME_NAMES = $(addprefix	$(COMPOSE_PROJECT_NAME)_, \
-# 							$(shell $(DKC_CONFIG) --volumes))  
-
-# IMAGES = $(shell $(DKC_CONFIG) --images)
-
-# all: install
-
-# up:
-# 	$(DKC) up -d
-
-# down:
-# 	$(DKC) down
-
-# logs:
-# 	$(DKC) logs -f
-
-# volumes:
-# 	mkdir -p $(VOLUME_PATHS)
-
-# info:
-# 	@echo -e "IMAGES:"
-# 	@$(DKC_CONFIG) --images
-# 	@echo -e "\nVOLUMES:"
-# 	@$(DKC_CONFIG) --volumes
-# 	@echo -e "\nCONTAINERS:"
-# 	@$(DKC) ps -a
-
-# build:
-# 	$(DKC) build --no-cache
-
-# install: volumes build
-# 	chmod +x ./srcs/tools/config.sh
-# 	sudo ./srcs/tools/config.sh ${DOMAIN_NAME}
-# 	$(DKC) create
-# 	@$(MAKE) -s info
-# 	@$(MAKE) up
-# 	# chmod -R 777 /home/${USER}/data
-
-# clean-volumes:
-# 	-docker volume rm $(VOLUME_NAMES)
-
-# fclean-volumes: clean-volumes
-# 	sudo rm -rf $(VOLUME_PATHS)
-
-# clean-images:
-# 	-docker rmi -f $(IMAGES)
-
-# fclean-images:
-# 	-docker rmi -f $(IMAGES)
-
-# clean-containers:
-# 	$(DKC) kill --remove-orphans
-# 	$(DKC) rm -f
-
-# clear:
-# 	echo 'docker stop $(docker ps -qa); docker rm $(docker ps -qa); docker rmi -f $(docker images -qa); docker volume rm $(docker volume ls -q); docker network rm $(docker network ls -q) 2>/dev/null'
-
-# clean: down clean-containers clean-volumes clean-images info
-# fclean: down clean-containers fclean-volumes fclean-images info
-# 	rm -rf /home/${USER}/data
-# re: down clean all
-# reset-all: fclean install all
+.PHONY: up down build new
--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,48 +1,29 @@
 #!/bin/sh
 
-# Check if the data directory exists
-if [ ! -d "/var/lib/mysql/wordpress" ]; then
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+# sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+
+# service mysql start
+set -e
+
+if ! systemctl is-active --quiet mariadb; then
+    rm -R /var/lib/mysql/*
+    mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
     service mysql start
-    service mysql start
-    mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME DEFAULT CHARACTER SET utf8;"
-    mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
-    mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
-    mysql -u root -e "FLUSH PRIVILEGES;"
-    mysqladmin -u root password $DB_ROOT_PASSWORD
-    service mysql stop
 fi
 
-/usr/bin/mysqld_safe
+cat > configure.sql << EOF
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+FLUSH PRIVILEGES;
+EOF
 
-# Start MariaDB in the background
-# mysqld --user=mysql --datadir=/var/lib/mysql &
+mariadb -uroot < ./configure.sql
 
-# Store the PID of the mysqld process
-# mysql_pid=$!
+rm ./configure.sql
 
-# Wait for MariaDB to become available
-# until mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} ping >/dev/null 2>&1; do
-#     sleep 1
-# done
+# service mysql stop
 
-# echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot -p
-# echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root -p
-# "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysqladmin shutdown
 
-# # ADMIN_PASSWORD=$(openssl passwd -1 "$WP_ADMIN_PASSWORD" | sed 's/\//\\\//g')
-# # PASSWORD=$(openssl passwd -1 "$WP_PASSWORD" | sed 's/\//\\\//g')
-
-# # # Initialize the database and permissions
-# # sudo mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot -p${MYSQL_ROOT_PASSWORD}
-# # sudo mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot -p${MYSQL_ROOT_PASSWORD}
-
-# if [ $? -ne 0 ]; then
-#          sudo mysql -u root -p${MYSQL_ROOT_PASSWORD} $MYSQL_DATABASE
-# fi
-
-# # Wait for the MariaDB process to shut down gracefully
-# wait "$mysql_pid"
-
-# # Start MariaDB in the foreground
-# exec mysqld --user=mysql
+exec mysqld_safe
