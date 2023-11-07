62bb521ffb25df64178629850c94d321e232da05
vgejno <vgejno@student.42lausanne.ch>
2023-11-07 23:26:03 +0100

    f

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+# Check if a marker file indicating installation completion exists
+if [ ! -f /var/lib/mysql/installed.txt ]; then
+    # The installation steps should be executed only once
 
-# Start the MySQL service
-service mysql start
+    # sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
-# Check if the service is not running
-if ! service mysql status | grep -q 'active (running)'; then
-    # If the service is not running, perform these actions
+    # Start the MySQL service
+    service mysql start
 
     # Initialize the MySQL database
     mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
@@ -27,12 +27,12 @@ EOF
 
     rm ./configure.sql
 
-    # Stop the MySQL service
-    # service mysql stop
-
-    # Shutdown the MySQL server
-    mysqladmin shutdown
+    # Create a marker file to indicate that installation has been completed
+    touch /var/lib/mysql/installed.txt
 fi
+
+# Start the MySQL server
+service mysql start
 
 # Shutdown the MySQL server (added this line at the end as well)
 mysqladmin shutdown
@@ -42,33 +42,78 @@ exec mysqld_safe
 
 
 
+
 # #!/bin/sh
 
 # # sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
+# # Start the MySQL service
 # service mysql start
-# # set -e
 
+# # Check if the service is not running
 # if ! service mysql status | grep -q 'active (running)'; then
+#     # If the service is not running, perform these actions
+
+#     # Initialize the MySQL database
 #     mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+
+#     # Start the MySQL service
 #     service mysql start
+
+#     # Create the database, user, and grant privileges
 #     cat > configure.sql << EOF
 #     CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 #     CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 #     GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 #     FLUSH PRIVILEGES;
-#     EOF
+# EOF
 
 #     mariadb -uroot < ./configure.sql
 
-
 #     rm ./configure.sql
 
+#     # Stop the MySQL service
 #     # service mysql stop
 
+#     # Shutdown the MySQL server
 #     mysqladmin shutdown
 # fi
+
+# # Shutdown the MySQL server (added this line at the end as well)
 # mysqladmin shutdown
 
-
+# # Start the MySQL server in safe mode
 # exec mysqld_safe
+
+
+
+# # #!/bin/sh
+
+# # # sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+
+# # service mysql start
+# # # set -e
+
+# # if ! service mysql status | grep -q 'active (running)'; then
+# #     mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+# #     service mysql start
+# #     cat > configure.sql << EOF
+# #     CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+# #     CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+# #     GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+# #     FLUSH PRIVILEGES;
+# #     EOF
+
+# #     mariadb -uroot < ./configure.sql
+
+
+# #     rm ./configure.sql
+
+# #     # service mysql stop
+
+# #     mysqladmin shutdown
+# # fi
+# # mysqladmin shutdown
+
+
+# # exec mysqld_safe
